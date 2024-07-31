bc6670f3c48b89b3730af18761a4aff251a7985b
Abhinav Kumar <abhinav@avitechlab.com>
2024-07-31 16:43:11 +0530

    fix build
    Signed-off-by: Abhinav Kumar <abhinav@avitechlab.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 // We use cryptographically strong PRNGs (crypto.getRandomBytes())
 // When using crypto.getRandomValues(), our primitive is hexString(),
 // from which we construct fraction().
-import RandomGenerator from './AbstractRandomGenerator.js';
-import createRandom from './createRandom.js';
-import NodeRandomGenerator from './NodeRandomGenerator.js';
+import RandomGenerator from "./AbstractRandomGenerator.js";
+import createRandom from "./createRandom.js";
+import NodeRandomGenerator from "./NodeRandomGenerator.js";
 
 const Random = createRandom(new NodeRandomGenerator());
 
-export { Random as default, type RandomGenerator };
+// CommonJS compatibility
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+	module.exports = Random;
+	module.exports.default = Random;
+}
+
+export { Random as default, RandomGenerator };
